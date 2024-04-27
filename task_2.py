8863e61031e02cb5622a621164f867c4df62e81c
Damian Mroz <damianmroz141@gmail.com>
2024-04-27 15:36:49 +0200

    Modify task_2.py

--- task_2.py
+++ task_2.py
@@ -3,7 +3,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 def user_initial():
-    print("Napisz gotowe, gdy wszystkie liczby do większego zadania zostały podane")
     x = input(
         "Podaj działanie, posługując się odpowiednią liczbą:"
         "\n"
@@ -24,59 +23,67 @@ def user_in():
     return a, b
 
 def user_in_extended():
-    z = ""
-    my_list = []
-    my_list.clear()
-    while z != "gotowe":
-        z = input("Dalsza liczba: ")
-        my_list.append((z))
-
-        try:
-            float(z)
-        except ValueError:
-            if z == "gotowe":
-                my_list.remove("gotowe")
-                break
-            print("to nie liczba")
-            exit()
-
-    return my_list
-
-def add():
-    in_values = [float(i) for i in user_in()]
-    in_values_extended = [float(i) for i in user_in_extended()]
-    logging.debug(f"Dodaję {in_values[0]} i {in_values[1]}")
-    if len(in_values_extended) > 0:
-        logging.debug(f"i dodaję {in_values_extended[::]} ")
-        
-    return f"Wynik to {sum(in_values_extended)+in_values[0]+in_values[1]}"
-
-def sub():
-    in_values = [float(i) for i in user_in()]
-    logging.debug(f"Odejmuję {in_values[0]} i {in_values[1]}")
-    diff = in_values[0] - in_values[1]
-
-    return f"Wynik to {diff}"
-
-def mul():
-    prod = 1
-    in_values = [float(i) for i in user_in()]
-    in_values_extended = [float(i) for i in user_in_extended()]
-    logging.debug(f"Multiplikuję {in_values[0]} i {in_values[1]}")
-    if len(in_values_extended) > 0:
-        logging.debug(f"i multiplikuję {in_values_extended[::]} ")
-    for x in in_values_extended:
-        prod = prod * x
-
-    return f"Wynik to {prod*in_values[0]*in_values[1]}"
-
-def div():
-    in_values = [float(i) for i in user_in()]
-    logging.debug(f"Dzielę {in_values[0]} i {in_values[1]}")
-    quo = in_values[0] / in_values[1]
+    z = input("Dalsze liczby: ")
+    z=z.split()
+    try:
+        for element in z:
+            float(element)
+            
+    except ValueError:
+        print("to nie liczba")
+        exit()
     
-    return f"Wynik to {quo}"
+    return z 
+
+a,b=user_in()
+
+numbers_list=[]
+if x=="1" or x=="3":
+    numbers_list=user_in_extended()
+
+def add(a,b,numbers_list):
+    sum=a+b
+    for element in numbers_list:
+        sum+=float(element)
+        
+    return sum
+
+def sub(a,b,numbers_list):
+    sub=a-b
+
+    return sub
+
+def mul(a,b,numbers_list):
+    prod=1
+    for element in numbers_list:
+        prod=prod*float(element)
+        
+    return prod*a*b
+    
+def div(a,b,numbers_list):
+    quo=a/b
+
+    return quo
 
 calculator = {"1": add, "2": sub, "3": mul, "4": div}
 
-print(calculator[x]())
+def add_info():
+    logging.debug(f"Dodaję {a} i {b}")
+    if len(numbers_list)> 0:
+        logging.debug(f"i dodaję {numbers_list}")
+
+def sub_info():
+    logging.debug(f"Odejmuję {a} i {b}")
+    
+def mul_info():
+    logging.debug(f"Multiplikuję {a} i {b}")
+    if len(numbers_list)> 0:
+        logging.debug(f"i multiplikuję {numbers_list}")
+
+def quo_info():
+    logging.debug(f"Dzielę {a} i {b}")
+    
+information= {"1":add_info,"2":sub_info,"3":mul_info, "4":quo_info}
+
+information[x]()
+print(f"Wynik to {calculator[x](a,b,numbers_list)}")
